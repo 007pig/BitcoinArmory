b250e577d6fa7223ecf6bf4dc297569b4c4d63f8
Xiaoyu Huang <xiaoyu.huang@btcchina.com>
2014-05-30 10:24:45 +0800

    more work

--- wallettools.py
+++ wallettools.py
@@ -15,26 +15,41 @@ class WalletTools(PyBtcWallet):
         shortLabel = self.labelName
         longLabel = self.labelDescr
 
-        newWallet.labelName = (shortLabel + ' (Part)')[:32]
-        newWallet.labelDescr = (longLabel + ' (Part copy)')[:256]
-
-        newWallet.addrMap['ROOT'] = self.addrMap['ROOT']
+        newWallet.labelName = (shortLabel + ' (Part 2)')[:32]
+        newWallet.labelDescr = (longLabel + ' (Part 2)')[:256]
 
         newWallet.commentsMap = self.commentsMap
         newWallet.opevalMap = self.opevalMap
 
-        newWallet.uniqueIDBin = self.uniqueIDBin
+        # newWallet.uniqueIDBin = self.uniqueIDBin
         newWallet.highestUsedChainIndex = self.highestUsedChainIndex
         newWallet.lastComputedChainAddr160 = self.lastComputedChainAddr160
         newWallet.lastComputedChainIndex = self.lastComputedChainIndex
 
-        addcount = 0
-        for addr160, addrObj in self.addrMap.iteritems():
-            addcount += 1
-            if (addcount >= 1000):
-                break
+        a1 = dict(self.addrMap.items()[0:5000])
+        a2 = dict(self.addrMap.items()[5000:10000])
+        a3 = dict(self.addrMap.items()[10000:15000])
 
-            newWallet.addrMap[addr160] = addrObj.copy()
+        newWallet.addrMap = a2
+        newWallet.addrMap['ROOT'] = self.addrMap['ROOT']
+
+        firstAddr = a2[a2.keys()[0]]
+
+        self.uniqueIDBin = (ADDRBYTE + firstAddr.getAddr160()[:5])[::-1]
+        self.uniqueIDB58 = binary_to_base58(self.uniqueIDBin)
+
+
+        # pprint.pprint(a1)
+        # pprint.pprint(a2)
+        # pprint.pprint(a3)
+
+        # addcount = 0
+        # for addr160, addrObj in self.addrMap.iteritems():
+        #     addcount += 1
+        #     if (addcount >= 10000):
+        #         break
+        #
+        #     newWallet.addrMap[addr160] = addrObj.copy()
             # newWallet.addrMap[addr160].binPrivKey32_Encr = SecureBinaryData()
             # newWallet.addrMap[addr160].binPrivKey32_Plain = SecureBinaryData()
             # newWallet.addrMap[addr160].binInitVector16 = SecureBinaryData()
@@ -47,9 +62,9 @@ class WalletTools(PyBtcWallet):
 
 
 
-wltLoad = WalletTools().readWalletFile('/Users/xiaoyu/Desktop/p1_armory_JQ3HSakw_.watchonly.wallet');
+wltLoad = WalletTools().readWalletFile('/Users/xiaoyu/Desktop/p1_armory_JQ3HSakw_.watchonly.wallet')
 wltID = wltLoad.uniqueIDB58
 
-wltLoad.forkWatchonlyWallet('/Users/xiaoyu/Library/Application Support/Armory/p1_armory_JQ3HSakw_.watchonly.part1.wallet')
+wltLoad.forkWatchonlyWallet('/Users/xiaoyu/Library/Application Support/Armory/p1_armory_JQ3HSakw_.watchonly.part2.wallet')
 
 pprint.pprint(wltID)
